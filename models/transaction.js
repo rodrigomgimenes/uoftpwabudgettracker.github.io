2752dc588b3b8d0e9766023a9918c760c1e399ce
Rodrigo Gimenes <rodrigomacedogimenes@gmail.com>
2020-06-04 12:17:08 -0400

    offline is working

--- models/transaction.js
+++ models/transaction.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-
-const Schema = mongoose.Schema;
+const Schema   = mongoose.Schema;
 
 const transactionSchema = new Schema(
   {
--- routes/api.js
+++ routes/api.js
@@ -1,34 +1,43 @@
-const router = require("express").Router();
-const Transaction = require("../models/transaction.js");
+// const router = require("express").Router();
+// const Transaction = require("../models/transaction.js");
+const db = require("../models");
 
-router.post("/api/transaction", ({body}, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
-});
+module.exports = function(app) {
+  // router.post("/api/transaction", ({body}, res) => {
+  app.post("/api/transaction", ({body}, res) => {
+    // Transaction.create(body)
+    db.Transaction.create(body)
+      .then(dbTransaction => {
+        res.json(dbTransaction);
+      })
+      .catch(err => {
+        res.status(404).json(err);
+      });
+  });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
-  Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
-});
+  // router.post("/api/transaction/bulk", ({body}, res) => {
+  app.post("/api/transaction/bulk", ({body}, res) => {
+    // Transaction.insertMany(body)
+    db.Transaction.insertMany(body)
+      .then(dbTransaction => {
+        res.json(dbTransaction);
+      })
+      .catch(err => {
+        res.status(404).json(err);
+      });
+  });
 
-router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
-});
+  // router.get("/api/transaction", (req, res) => {
+  app.get("/api/transaction", (req, res) => {
+    // Transaction.find({}).sort({date: -1})
+    db.Transaction.find({}).sort({date: -1})
+      .then(dbTransaction => {
+        res.json(dbTransaction);
+      })
+      .catch(err => {
+        res.status(404).json(err);
+      });
+  });
+};
 
-module.exports = router;
+// module.exports = router;
